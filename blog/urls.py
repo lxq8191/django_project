c74c3b54934e319e15b9cdb3929eb1c9723677e6
lxq8191 <6051183@163.com>
2017-06-10 12:56:31 +0800

    third commit

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^user/',views.user_page,name='user_page')
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+	#将帖子按照发表时间排序
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts': posts})
+def user_page(request):
+	return render(request, 'blog/user_page.html', {})
